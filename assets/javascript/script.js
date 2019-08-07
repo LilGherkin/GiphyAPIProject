733c9064325d67285412e1e56d9266a9528f1d3f
Mark Hartman <mthartman77@gmail.com>
2019-08-06 23:24:36 -0500

    Added in the ability to pull stuff from Giphy.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,28 @@
 // API KEY: IkUqnuOr2U0TMXYMPulxtaXuIwprxIoK
 
-var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=IkUqnuOr2U0TMXYMPulxtaXuIwprxIoK&tag=cats";
+$("button").on("click", function(){
+    var creature = $(this).attr("data-creature");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + creature + "&api_key=IkUqnuOr2U0TMXYMPulxtaXuIwprxIoK&limit=10";
+    
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+    
+        console.log(response);
+
+        var results = response.data
+
+        for (var i=0; i < results.length; i++){
+            var CreatureDiv = $("<div style='float:left;padding:10px'>");
+            var CreatureImg = $("<img>");
+            var Rating = results[i].rating;
+            var RatingWriter = $("<p>").html("Rating: " + "<b>" + Rating + "</b>");
+            CreatureImg.attr("src", results[i].images.fixed_height.url);
+            CreatureDiv.prepend(CreatureImg);
+            CreatureDiv.prepend(RatingWriter);
+            $("#GifsHere").prepend(CreatureDiv);
+        }
+      });
+});
